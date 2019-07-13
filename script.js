37bfe1ccbc22f17e2e696b2578c86404e11516ea
elab-developer <44549238+elab-developer@users.noreply.github.com>
2019-07-13 14:07:15 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var slideIndex = 0;
+var slideIndex = 0 , reviewIndex = 0;
 showSlides();
+showReview();
 
 function showSlides() {
   var i;
@@ -16,4 +17,17 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";  
 //  dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+function showReview(){
+    
+    var i;
+    var review = document.getElementsByClassName("blockquote");
+    for(i = 0 ; i< review.length; i++){
+        review[i].style.display = "none";
+    }
+    reviewIndex ++;
+    if(reviewIndex > review.length) {reviewIndex = 2}
+    review[reviewIndex - 1].style.display = "block";
+    setTimeout(showReview , 3000);    
 }
