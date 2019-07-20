a59394a92b6357716c7e1ba4cf96a3996c4d634b
elab-developer <44549238+elab-developer@users.noreply.github.com>
2019-07-20 14:01:46 +0530

    Add files via upload

---
+++ booknow.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.submit_btn').click(function(event){
+        event.preventDefault()
+        
+        var name = $('.name').val()
+        var pilgrimsCount = $('.pilgrimsCount').val()
+        var address = $('.address').val()
+        var phone = $('.phone').val()
+        var email = $('.email').val()
+        var message = $('.message').val()
+        var payment = $('.payment').val()
+        
+    })
+})
